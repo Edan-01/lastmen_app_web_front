74c2fadd2f825a92a8a7b07a40c0e163c5066f58
Aron Ticsihua Alfaro <aronticsihua1101@gmail.com>
2023-01-13 16:01:30 -0500

    Cambio de diseño de la boleta

--- src/app/views/mantenimiento/ventas/ventas-comprobante/ventas-comprobante.component.ts
+++ src/app/views/mantenimiento/ventas/ventas-comprobante/ventas-comprobante.component.ts
@@ -6,6 +6,7 @@ import { VentasModel } from 'src/app/models/ventas.model';
 import { ClientesService } from 'src/app/service/clientes.service';
 import { ProductoService } from 'src/app/service/producto.service';
 import { UsuarioService } from 'src/app/service/usuario.service';
+import { DetalleVentaModel } from 'src/app/models/detalleVentas.model';
 import html2canvas from 'html2canvas';
 
 @Component({
@@ -16,6 +17,7 @@ import html2canvas from 'html2canvas';
 export class VentasComprobanteComponent implements OnInit {
   @Input() venta: VentasModel = new VentasModel();
 
+  detalleVenta: DetalleVentaModel= new DetalleVentaModel();
   cliente: ClienteModel = new ClienteModel();
   usuario: UsuarioModel = new UsuarioModel();
   producto: ProductoModel[] = [];
@@ -91,43 +93,43 @@ export class VentasComprobanteComponent implements OnInit {
   //   mywindow.print();
   // }
 
-  // CANVAS
-  PrintElem() {
-    var elem = document.getElementById("app2");
-    if (!elem) {
-      alert("No se encontró el elemento con id 'app2'");
-      return;
-    }
-    var mywindow: any = window.open('', 'PRINT', 'height=1000,width=800');
-    let app2 = document.getElementById('app2');
-    if (app2) {
-      mywindow.document.write('<html><head><title>' + document.title + '</title>');
-      mywindow.document.write('</head><body >');
-      mywindow.document.write('<h1>' + document.title + '</h1>');
-      html2canvas(elem, {allowTaint: true}).then(canvas => {
-      var imgData = canvas.toDataURL('image/png');
-      mywindow.document.write('<img src="' + imgData + '" />');
-      mywindow.document.write('</body></html>');
-      mywindow.focus();
-      mywindow.print();
-      mywindow.document.close(); 
-      return true;
-      });
-    }
-  }
-
-    // ORIGINAL
+  // // CANVAS
   // PrintElem() {
-  //   var mywindow: any = window.open('', 'PRINT', 'height=400,width=600');
+  //   var elem = document.getElementById("app2");
+  //   if (!elem) {
+  //     alert("No se encontró el elemento con id 'app2'");
+  //     return;
+  //   }
+  //   var mywindow: any = window.open('', 'PRINT', 'height=1000,width=800');
   //   let app2 = document.getElementById('app2');
   //   if (app2) {
-  //     let html = app2.innerHTML; //This      
-  //     mywindow.document.write(html);
-  //   }    
-
-  //   mywindow.document.close();
-  //   mywindow.focus();
-  //   mywindow.print();
+  //     mywindow.document.write('<html><head><title>' + document.title + '</title>');
+  //     mywindow.document.write('</head><body >');
+  //     mywindow.document.write('<h1>' + document.title + '</h1>');
+  //     html2canvas(elem, {allowTaint: true}).then(canvas => {
+  //     var imgData = canvas.toDataURL('image/png');
+  //     mywindow.document.write('<img src="' + imgData + '" />');
+  //     mywindow.document.write('</body></html>');
+  //     mywindow.focus();
+  //     mywindow.print();
+  //     mywindow.document.close(); 
+  //     return true;
+  //     });
+  //   }
   // }
+
+   //  ORIGINAL
+   PrintElem() {
+     var mywindow: any = window.open('', 'PRINT', 'height=400,width=600');
+     let app2 = document.getElementById('app2');
+     if (app2) {
+       let html = app2.innerHTML; this      
+       mywindow.document.write(html);
+     }    
+
+     mywindow.document.close();
+     mywindow.focus();
+     mywindow.print();
+   }
 }
 
